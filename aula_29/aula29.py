6ae3f2d8a52e7d17aca26f12cfed1c8b9d1fce34
brancherbruno <brunodesouzabrancher@gmail.com>
2022-10-10 20:21:42 -0300

    resolução do exercício aula 29

---
+++ aula_29/aula29.py
@@ -0,0 +1,15 @@
+# PASS E ELLIPSIS COM PLACEHOLDER
+
+# EXEMPLO
+
+valor = False
+
+if valor:
+    pass  # nesse caso o pass não vai mostrar nada em tela (se o valor for True) e serve para deixar um bloco em branco
+          # caso o dev deixe algo em branco para ser programado depois.
+          # pode ser substituído por 3 pontinhos (...)
+else:
+    print("Acabou")
+
+
+
