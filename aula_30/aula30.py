2794a546c4f8c1c6ea808d11d2e167d0870dea28
brancherbruno <brunodesouzabrancher@gmail.com>
2022-10-11 23:40:12 -0300

    resolução do exercício aula 30

--- aula_30/aula30.py
+++ aula_30/aula30.py
@@ -22,16 +22,19 @@ boa noite 18-23;
 """
 
 
-h = input("Digite a hora: ")
+h = input("Digite a hora [0-23]: ")
 
 if h.isdigit():
     h = int(h)
-    if (h >= 0) and (h <= 11):
-        print('Bom dia')
-    elif (h >= 12) and (h <= 17):
-        print('Boa tarde')
+    if (h < 0) or (h > 23):
+        print('O horário deverá estar entre 0 e 23h')
     else:
-        print('Boa noite')
+        if (h >= 0) and (h <= 11):
+            print('Bom dia')
+        elif (h >= 12) and (h <= 17):
+            print('Boa tarde')
+        else:
+            print('Boa noite')
 else:
     print('O valor não é um horário válido')
 
@@ -43,10 +46,11 @@ se tiver mais de 6, seu nome é muito longo;
 """
 
 n = str(input('Digite seu nome: '))
+tam = len(n)
 
-if len(n) < 4:
+if tam < 4:
     print('Seu nome é muito curto')
-elif (len(n) >= 5) and (len(n) <= 6):
+elif (tam >= 5) and (tam <= 6):
     print('Seu nome tem tamanho justo')
 else:
     print('Seu nome é muito longo')
