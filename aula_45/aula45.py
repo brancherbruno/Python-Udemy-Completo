88c3a46ceb500a7e86278f31c08f7755d98e7b0d
brancherbruno <brunodesouzabrancher@gmail.com>
2022-10-24 19:43:50 -0300

    resolução do exercício aula 44 e 45

--- aula_45/aula45.py
+++ aula_45/aula45.py
@@ -2,8 +2,28 @@
 Operador Ternário
 """
 
+log_user = True  # para mostrar logado, basta colocar como TRUE
+'''
+if log_user:
+    msg = 'Usuário logado.'
+else:
+    msg = 'Usuário não logado.'
 
+print(msg)
 
+'''
+'''
+msg = 'Usuário logado.' if log_user else 'Usuário precisa logar.'
+print(msg)
+'''
 
+idd = input('Qual sua idade? ')
+if not idd.isnumeric():
+    print('Você deve digitar somente números. Tente novamente.')
+else:
+    idd = int(idd)
+    maior = (idd >= 18)
+    msg = 'Pode logar.' if maior else 'Você é menor de idade.'
 
+    print(msg)
 
