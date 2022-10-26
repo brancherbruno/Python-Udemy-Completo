73e3e33a1da60344d7240568e14a41df0513faa1
brancherbruno <brunodesouzabrancher@gmail.com>
2022-10-25 22:51:13 -0300

    resolução do exercício aula 46 e 47

---
+++ aula_46/aula46.py
@@ -0,0 +1,23 @@
+"""
+Expressão condicional com operador OR
+"""
+
+a = ''
+b = []
+c = {}
+d = 0
+e = None
+f = False
+g = 'Bruno'
+
+var = a or b or c or d or e or f or g
+
+print(var)
+
+
+
+
+
+
+
+
---
+++ aula_47/aula47.py
@@ -0,0 +1,9 @@
+positivo = 0
+negativo = 10
+
+while positivo < 11 and negativo >= 0:
+    print(f'{positivo}  {negativo}')
+    positivo += 1
+    negativo -= 1
+
+
